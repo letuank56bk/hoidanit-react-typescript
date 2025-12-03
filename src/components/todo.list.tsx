acce1fbcd1b651953f399b3e711c17346777aadf
Alan Le <letuank56bk@gmail.com>
2025-12-03 21:55:18 +0700

    Video 23 - Props

--- src/components/todo.list.tsx
+++ src/components/todo.list.tsx
@@ -1,7 +1,36 @@
+import TodoData from "./todo.data";
+
 const TodoList = () => {
+    //hardcode data tại component cha
+    const todos = [
+        {
+            id: 1,
+            title: "Learn React TypeScript",
+            isComplete: false
+        },
+        {
+            id: 2,
+            title: "Subscribe Youtube HoiDanIT",
+            isComplete: true
+        },
+        {
+            id: 3,
+            title: "Learn English",
+            isComplete: true
+        },
+    ]
+
+
     return (
         <>
-            <div>Todo list tsx</div>
+            <div>My Todo list:</div>
+            <br />
+            <TodoData
+                todos={todos}
+                owner={"HoiDanIT"}
+                age={25}
+                isDeveloper={true}
+            />
         </>
     )
 }
